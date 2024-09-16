b06df59dda1d17d4efa6b21e50812ea8ffb3090d
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2024-09-16 21:19:09 +0300

    add router icons

--- dictionary/App.tsx
+++ dictionary/App.tsx
@@ -6,6 +6,7 @@ import Translate from './screens/Translate';
 import { NavigationContainer } from '@react-navigation/native';
 import Dictionary from './screens/Dictionary';
 import { colors } from './assets/styles/style';
+import { AntDesign } from '@expo/vector-icons';
 
 const TabRouter = createBottomTabNavigator()
 
@@ -13,9 +14,21 @@ export default function App() {
   return (
     <NavigationContainer>
         <TabRouter.Navigator>
-          <TabRouter.Screen name="Home" component={Home}/>
-          <TabRouter.Screen name="Translate" component={Translate}/>
-          <TabRouter.Screen name="Dictionary" component={Dictionary}/>
+          <TabRouter.Screen name="Home" component={Home} options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <AntDesign name="home" size={24} color="black" />
+            ),
+          }}/>
+          <TabRouter.Screen name="Translate" component={Translate} options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <AntDesign name="swap" size={24} color="black" />
+            ),
+          }}/>
+          <TabRouter.Screen name="Dictionary" component={Dictionary} options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <AntDesign name="book" size={24} color="black" />
+            ),
+          }}/>
         </TabRouter.Navigator>
     </NavigationContainer>
   );
